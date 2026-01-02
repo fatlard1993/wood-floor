bb28409e6b91cbdc5a896dfe5094d36bbe0bbfe7
justfatlard <fatlard1993@gmail.com>
2026-01-01 16:43:37 -0800

    update for 1.21.11

--- src/main/java/justfatlard/wood_floor/Main.java
+++ src/main/java/justfatlard/wood_floor/Main.java
@@ -1,37 +1,105 @@
 package justfatlard.wood_floor;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class Main implements ModInitializer {
 	public static final String MOD_ID = "wood-floor-justfatlard";
 
-	public static final WoodFloor ACACIA_FLOOR = new WoodFloor();
-	public static final WoodFloor BIRCH_FLOOR = new WoodFloor();
-	public static final WoodFloor DARK_OAK_FLOOR = new WoodFloor();
-	public static final WoodFloor JUNGLE_FLOOR = new WoodFloor();
-	public static final WoodFloor OAK_FLOOR = new WoodFloor();
-	public static final WoodFloor SPRUCE_FLOOR = new WoodFloor();
+	// Helper to create block settings with registry key
+	private static AbstractBlock.Settings createSettings(String name) {
+		RegistryKey<Block> key = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(MOD_ID, name));
+		return AbstractBlock.Settings.create()
+			.registryKey(key)
+			.strength(2.0f, 3.0f)
+			.sounds(BlockSoundGroup.WOOD)
+			.burnable();
+	}
+
+	// Original woods
+	public static final WoodFloor ACACIA_FLOOR = new WoodFloor(createSettings("acacia_floor"));
+	public static final WoodFloor BIRCH_FLOOR = new WoodFloor(createSettings("birch_floor"));
+	public static final WoodFloor DARK_OAK_FLOOR = new WoodFloor(createSettings("dark_oak_floor"));
+	public static final WoodFloor JUNGLE_FLOOR = new WoodFloor(createSettings("jungle_floor"));
+	public static final WoodFloor OAK_FLOOR = new WoodFloor(createSettings("oak_floor"));
+	public static final WoodFloor SPRUCE_FLOOR = new WoodFloor(createSettings("spruce_floor"));
+
+	// Nether woods (1.16)
+	public static final WoodFloor CRIMSON_FLOOR = new WoodFloor(createSettings("crimson_floor"));
+	public static final WoodFloor WARPED_FLOOR = new WoodFloor(createSettings("warped_floor"));
+
+	// Mangrove (1.19)
+	public static final WoodFloor MANGROVE_FLOOR = new WoodFloor(createSettings("mangrove_floor"));
+
+	// Cherry & Bamboo (1.20)
+	public static final WoodFloor CHERRY_FLOOR = new WoodFloor(createSettings("cherry_floor"));
+	public static final WoodFloor BAMBOO_FLOOR = new WoodFloor(createSettings("bamboo_floor"));
+	public static final WoodFloor BAMBOO_MOSAIC_FLOOR = new WoodFloor(createSettings("bamboo_mosaic_floor"));
+
+	// Pale Oak (1.21.4)
+	public static final WoodFloor PALE_OAK_FLOOR = new WoodFloor(createSettings("pale_oak_floor"));
 
 	private static void register(String name, Block block){
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID, name), new BlockItem(block, new Item.Settings().group(ItemGroup.DECORATIONS)));
+		Identifier id = Identifier.of(MOD_ID, name);
 
-		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, name), block);
+		Registry.register(Registries.BLOCK, id, block);
+
+		RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, id);
+		Registry.register(Registries.ITEM, id, new BlockItem(block, new Item.Settings().registryKey(itemKey)));
 	}
 
 	@Override
 	public void onInitialize(){
+		// Original woods
 		register("acacia_floor", ACACIA_FLOOR);
 		register("birch_floor", BIRCH_FLOOR);
 		register("dark_oak_floor", DARK_OAK_FLOOR);
 		register("jungle_floor", JUNGLE_FLOOR);
 		register("oak_floor", OAK_FLOOR);
 		register("spruce_floor", SPRUCE_FLOOR);
+
+		// Nether woods (1.16)
+		register("crimson_floor", CRIMSON_FLOOR);
+		register("warped_floor", WARPED_FLOOR);
+
+		// Mangrove (1.19)
+		register("mangrove_floor", MANGROVE_FLOOR);
+
+		// Cherry & Bamboo (1.20)
+		register("cherry_floor", CHERRY_FLOOR);
+		register("bamboo_floor", BAMBOO_FLOOR);
+		register("bamboo_mosaic_floor", BAMBOO_MOSAIC_FLOOR);
+
+		// Pale Oak (1.21.4)
+		register("pale_oak_floor", PALE_OAK_FLOOR);
+
+		// Add all floors to creative tab
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> {
+			entries.add(ACACIA_FLOOR);
+			entries.add(BIRCH_FLOOR);
+			entries.add(DARK_OAK_FLOOR);
+			entries.add(JUNGLE_FLOOR);
+			entries.add(OAK_FLOOR);
+			entries.add(SPRUCE_FLOOR);
+			entries.add(CRIMSON_FLOOR);
+			entries.add(WARPED_FLOOR);
+			entries.add(MANGROVE_FLOOR);
+			entries.add(CHERRY_FLOOR);
+			entries.add(BAMBOO_FLOOR);
+			entries.add(BAMBOO_MOSAIC_FLOOR);
+			entries.add(PALE_OAK_FLOOR);
+		});
 
 		System.out.println("Loaded wood-floor");
 	}
--- src/main/java/justfatlard/wood_floor/WoodFloor.java
+++ src/main/java/justfatlard/wood_floor/WoodFloor.java
@@ -1,10 +1,10 @@
 package justfatlard.wood_floor;
 
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Material;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.Waterloggable;
-import net.minecraft.entity.EntityContext;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.state.StateManager;
@@ -12,16 +12,18 @@ import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.IWorld;
+import net.minecraft.world.WorldView;
+import net.minecraft.world.tick.ScheduledTickView;
 
 public class WoodFloor extends Block implements Waterloggable {
 	public static final BooleanProperty WATERLOGGED;
 	protected static final VoxelShape SHAPE;
 
-	public WoodFloor(){
-		super(Block.Settings.of(Material.WOOD));
+	public WoodFloor(AbstractBlock.Settings settings){
+		super(settings);
 
 		this.setDefaultState(this.getDefaultState().with(WATERLOGGED, false));
 	}
@@ -29,19 +31,20 @@ public class WoodFloor extends Block implements Waterloggable {
 	protected void appendProperties(StateManager.Builder<Block, BlockState> builder){ builder.add(WATERLOGGED); }
 
 	public FluidState getFluidState(BlockState state) {
-		return (Boolean)state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
-	}
-
-	public BlockState getStateForNeighborUpdate(BlockState state, Direction facing, BlockState neighborState, IWorld world, BlockPos pos, BlockPos neighborPos) {
-		if ((Boolean)state.get(WATERLOGGED)) {
-			world.getFluidTickScheduler().schedule(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
-		}
-
-		return super.getStateForNeighborUpdate(state, facing, neighborState, world, pos, neighborPos);
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
 	}
 
 	@Override
-	public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, EntityContext context){
+	public BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView, BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, Random random) {
+		if (state.get(WATERLOGGED)) {
+			tickView.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+		}
+
+		return super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
+	}
+
+	@Override
+	public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context){
 		return SHAPE;
 	}
 
